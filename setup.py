8c8f528d7d8230bbd308bc553d7f544c035e47ba
unknown <Jeevan Pillay>
2024-10-16 17:40:39 +1100

    something wrong w publish

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="jp-mesh-processing",
-    version="0.1.1",
+    name="jp_mesh",
+    version="0.1.0",
     author="jeevanpillay",
     author_email="jp@jeevanpillay.com",
     description="A package for 3D mesh processing and analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jeevanpillay/jp-mesh-processing",
+    url="https://github.com/jeevanpillay/jp-mesh",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     classifiers=[
@@ -20,11 +20,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
     install_requires=[
         "numpy",
         "trimesh",
